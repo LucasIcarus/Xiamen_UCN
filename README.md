b987038613e0e1f4ce680d43fc64880ccc9a482e
Fancy <lucasicarus@hotmail.com>
2016-03-25 22:09:52 +0800

    Home 16.03.25 Recombination

---
+++ README.md
@@ -0,0 +1,29 @@
+## Requested By Someone from Academy of Arts
+
+<a href="https://lucasicarus.github.io/Xiamen_UCN/">View The Semi-finished Web Pages Here →</a>
+
+## 16.03.01
+
+#### Get the request from my GF, and can code something for a living is great.
+
+#### Preparing to run it base on bootstrap and jekyll, after I hear about that the pure static pages are enough.
+
+#### Go with the structure thing and being slow down in the Lab.
+
+## 16.03.07
+
+#### The basic web pages are set up.
+
+#### Waiting for some feedback.
+
+## 16.03.15
+
+#### Change the first edition of demo to take the business owner's fancy. Haha, fancy is a magical word.
+
+#### Done as the sundown. Send the 2nd edition to her.
+
+## 16.03.24
+
+#### Job-hunting time. This poor little thing may help?
+
+#### Recombination is needed for my several demos. Right on to it.
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    htmlmin = require('gulp-htmlmin'),
+    cleanCSS = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    pngquant = require('imagemin-pngquant');
+    
+
+gulp.task('css', function () {
+    return gulp.src('./css/main.css')
+        .pipe(cleanCSS({compability: 'ie8'}))
+        .pipe(autoprefixer({
+            browsers: ['> 1%', 'IE 7'],
+            cascade: false
+        }))
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('img', function () {
+    return gulp.src('./_img/**.*')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./img'));
+});
+
+gulp.task('html', function () {
+    return gulp.src('./_src/*.html')
+        .pipe(htmlmin({collapseWhiteSpace: true}))
+        .pipe(gulp.dest('./'));
+})
+
